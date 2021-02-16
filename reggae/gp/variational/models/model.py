a2b00f42c1a24e3dd83a984ad86f5dbb6cbaf0c3
Jacob Moss <cob.mossy@gmail.com>
2021-02-16 11:37:04 +0000

    cleanup model

--- reggae/gp/variational/models/model.py
+++ reggae/gp/variational/models/model.py
@@ -119,19 +119,16 @@ class VariationalLFM(LFM):
         # Precompute variables
         self.Kmm = self.rbf(self.inducing_inputs)
         self.L = torch.cholesky(self.Kmm)
-        # self.inv_Kmm = cholesky_inverse(self.L)
         q_cholS = torch.tril(self.q_cholS)
         self.S = torch.matmul(q_cholS, torch.transpose(q_cholS, 1, 2))
 
+        # Integrate forward from the initial positions h0.
         h0 = self.initial_state(h)
-        # Integrate forward from the initial positions h.
         h_samples = odeint(self.odefunc, h0, t, method='dopri5', rtol=rtol, atol=atol)  # (T, S, num_outputs, 1)
 
-        h_avg = torch.mean(h_samples, dim=1)
-        h_std = torch.std(h_samples, dim=1)
+        h_out = torch.mean(h_samples, dim=1).transpose(0, 1)
+        h_std = torch.std(h_samples, dim=1).transpose(0, 1)
 
-        h_out = torch.transpose(h_avg, 0, 1)
-        h_std = torch.transpose(h_std, 0, 1)
         if compute_var:
             return self.decode(h_out), h_std
         return self.decode(h_out)
@@ -162,42 +159,23 @@ class VariationalLFM(LFM):
         Parameters:
             t: shape (T*,)
         """
-        ## Uncomment this if calling get_tfs on a fresh model
-        # self.Kmm = self.rbf(self.inducing_inputs)
-        # L = torch.cholesky(self.Kmm)
-        # self.inv_Kmm = cholesky_inverse(L)
-        # q_cholS = torch.tril(self.q_cholS)
-        # self.S = torch.matmul(q_cholS, torch.transpose(q_cholS, 1, 2))
-        ##
-        if t.shape[0] == 1 and self.extra_points > 0:
-            # t_new = torch.ones(3)
-            # t_new[0] = t[0]-0.05
-            # t_new[2] = t[0]+0.05
-            # t_new[1] = t[0]
-            t_l = list()
-            for i in range(self.extra_points, 0, -1):
-                t_l.append(t[0]-0.05*i)
-            for i in range(self.extra_points+1):
-                t_l.append(t[0]+0.05*i)
-            t = torch.tensor(t_l, device=t.device).reshape(-1)
-            # t = torch.tensor([t[0]-0.05, t[0], t[0]+0.05]).reshape(-1)
-            # print(t)
         Ksm = self.rbf(t, self.inducing_inputs)  # (I, T*, Tu)
-        # α_1 = torch.matmul(Ksm, self.inv_Kmm)  # (I, T*, Tu)
-        α = torch.cholesky_solve(Ksm.permute([0, 2, 1]), self.L, upper=False).permute([0, 2, 1])    # (I, T*, Tu)
-        m_s = torch.matmul(α, self.q_m)  # (I, T*, Tu)
+        α = torch.cholesky_solve(Ksm.permute([0, 2, 1]), self.L, upper=False).permute([0, 2, 1])  # (I, T*, Tu)
+        m_s = torch.matmul(α, self.q_m)  # (I, T*, 1)
+        m_s = torch.squeeze(m_s, 2)
         Kss = self.rbf(t)  # (I, T*, T*)
-        S_Kmm = self.S - self.Kmm # (I, Tu, Tu)
-        AS_KA = torch.matmul(torch.matmul(α, S_Kmm), torch.transpose(α, 1, 2)) # (I, T*, T*)
-        S_s = (Kss + AS_KA) # (I, T*, T*)
+        S_Kmm = self.S - self.Kmm  # (I, Tu, Tu)
+        AS_KA = torch.matmul(torch.matmul(α, S_Kmm), torch.transpose(α, 1, 2))  # (I, T*, T*)
+        S_s = (Kss + AS_KA)  # (I, T*, T*)
+        # print('ss', m_s.shape, S_s.shape)
 
         if S_s.shape[2] > 1:
             if True:
                 jitter = 1e-5 * torch.eye(S_s.shape[1], dtype=S_s.dtype)
                 S_s = S_s + jitter
-            q_f = MultivariateNormal(torch.squeeze(m_s, 2), S_s)
+            q_f = MultivariateNormal(m_s, S_s)
         else:
-            q_f = Normal(torch.squeeze(m_s), torch.squeeze(S_s))
+            q_f = Normal(m_s, torch.squeeze(S_s, 2))
 
         return q_f
 
