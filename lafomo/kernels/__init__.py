c877c84ae7067cd2be00d80eae44ea83bec0ce5f
Jacob Moss <cob.mossy@gmail.com>
2021-03-15 12:32:30 +0000

    add spatiotemporal RBF kernel

--- lafomo/kernels/__init__.py
+++ lafomo/kernels/__init__.py
@@ -1,8 +1,9 @@
 from lafomo.kernels.sim import SIMKernel, SIMMean
-from lafomo.kernels.stationary import RBF
+from lafomo.kernels.stationary import RBF, SpatioTemporalRBF
 
 __all__ = [
     'SIMKernel',
     'SIMMean',
     'RBF',
+    'SpatioTemporalRBF',
 ]
---
+++ lafomo/variational/models/partial.py
@@ -0,0 +1,46 @@
+from fenics import *
+from fenics_adjoint import *
+
+import torch_fenics
+
+
+class ReactionDiffusion(torch_fenics.FEniCSModule):
+    def __init__(self, dt, mesh_cells):
+        """
+        Parameters:
+            dt: scalar float
+            mesh_cells: Number of cells in the spatial mesh
+        """
+        super().__init__()
+        self.dt = dt
+        # Create function space
+        mesh = UnitIntervalMesh(mesh_cells)
+        self.V = FunctionSpace(mesh, 'P', 1)
+
+        # Create trial and test functions
+        y = TrialFunction(self.V)
+        self.v = TestFunction(self.V)
+
+
+    def solve(self, y_prev, u, sensitivity, decay, diffusion):
+        # Construct bilinear form (Arity = 2 (for both Trial and Test function))
+        y = TrialFunction(self.V)
+        self.a = (1 + self.dt * decay) * y * self.v * dx
+        self.a += self.dt * diffusion * inner(grad(y), grad(self.v)) * dx
+
+        # Construct linear form
+        L = (y_prev + self.dt * sensitivity * u) * self.v * dx
+
+        # Construct boundary condition
+        bc = DirichletBC(self.V, Constant(0), 'on_boundary')
+
+        # Solve the Poisson equation
+        y = Function(self.V)
+        solve(self.a == L, y, bc)
+
+        return y
+
+    def input_templates(self):
+        # Declare templates for the inputs to Poisson.solve
+        return Function(self.V), Function(self.V), \
+               Constant(0), Constant(0), Constant(0)
