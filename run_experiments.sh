c44727ecfcf81190a2511d0ff0055ef9788b1f9d
Jacob Moss <jm2311@kiiara.cl.cam.ac.uk>
2021-04-01 17:37:01 +0100

    merged

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,5 @@
 python -m experiments.run --data dros-kr
 #python -m experiments.run --data dros-kni
 #python -m experiments.run --data dros-gt
-python -m experiments.run --data toy-spatial
+#python -m experiments.run --data toy-spatial
+
