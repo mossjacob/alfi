fix gradient matching

lafomo/models/ordinary_lfm.py
@@ -41,17 +41,28 @@ class OrdinaryLFM(VariationalLFM):
         self.nfe = 0
 
         # Get GP outputs
-        t_f = torch.arange(t.min(), t.max()+step_size/3, step_size/3)
+        if self.pretrain_mode:
+            t_f = t[0]
+            h0 = t[1]
+        else:
+            t_f = torch.arange(t.min(), t.max()+step_size/3, step_size/3)
+            h0 = self.initial_state()
+            h0 = h0.unsqueeze(0).repeat(self.config.num_samples, 1, 1)
+
         q_f = self.gp_model(t_f)
-        # Integrate forward from the initial positions h0.
-        h0 = self.initial_state()
-        h0 = h0.unsqueeze(0).repeat(self.config.num_samples, 1, 1)
+
+
         self.f = q_f.rsample(torch.Size([self.config.num_samples])).permute(0, 2, 1)  # (S, I, T)
         self.f = self.G(self.f)
-        self.t_index = 0
-        self.last_t = self.f.min()-1
 
-        h_samples = odeint(self.odefunc, h0, t, method='rk4', options=dict(step_size=step_size)) # (T, S, num_outputs, 1)
+        if self.pretrain_mode:
+            h_samples = self.odefunc(t_f, h0)
+            h_samples = h_samples.permute(2, 0, 1)
+        else:
+            # Integrate forward from the initial positions h0.
+            self.t_index = 0
+            self.last_t = self.f.min() - 1
+            h_samples = odeint(self.odefunc, h0, t, method='rk4', options=dict(step_size=step_size)) # (T, S, num_outputs, 1)
 
         self.f = None
         # self.t_index = None
@@ -64,7 +75,6 @@ class OrdinaryLFM(VariationalLFM):
         h_mean = self.decode(h_mean)
         # TODO: make distribution something less constraining
         h_covar = torch.diag_embed(h_var)
-
         batch_mvn = gpytorch.distributions.MultivariateNormal(h_mean, h_covar)
         return gpytorch.distributions.MultitaskMultivariateNormal.from_batch_mvn(batch_mvn, task_dim=0)
 

lafomo/models/variational_lfm.py
@@ -24,12 +24,13 @@ class VariationalLFM(LFM, ABC):
                  num_outputs: int,
                  gp_model: ApproximateGP,
                  config: VariationalConfiguration,
-                 num_training_points = None,
+                 num_training_points=None,
                  dtype=torch.float64):
         super().__init__()
         self.gp_model = gp_model
         self.num_outputs = num_outputs
         self.likelihood = MultitaskGaussianLikelihood(num_tasks=self.num_outputs)
+        self.pretrain_mode = False
         try:
             self.inducing_points = self.gp_model.get_inducing_points()
         except AttributeError:
@@ -84,9 +85,13 @@ class VariationalLFM(LFM, ABC):
         self.gp_model.train(mode)
         self.likelihood.train(mode)
 
+    def pretrain(self, mode=True):
+        self.pretrain_mode = mode
+
     def eval(self):
         self.gp_model.eval()
         self.likelihood.eval()
+        self.pretrain(False)
 
     def predict_m(self, t_predict, **kwargs) -> torch.distributions.MultivariateNormal:
         """