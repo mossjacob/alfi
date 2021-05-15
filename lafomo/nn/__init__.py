559aced6a2de1eb321d6ea345a7414e0e2cf8198
Jacob Moss <cob.mossy@gmail.com>
2021-05-15 16:00:57 +0100

    neural operator refactoring

--- lafomo/nn/__init__.py
+++ lafomo/nn/__init__.py
@@ -1,6 +1,7 @@
 from .spectral_conv import SpectralConv1d
 from .conv_block import SimpleBlock1d, SimpleBlock2d
 from .lp_loss import LpLoss
+from .encoders import MuSigmaEncoder
 
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     'SimpleBlock2d',
     'SpectralConv1d',
     'LpLoss',
+    'MuSigmaEncoder',
 ]
