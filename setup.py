a04cd96ab576f906e474141778bbbb166f572288
Jacob Moss <cob.mossy@gmail.com>
2021-06-02 10:44:31 +0100

    rename in config to alfi

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lafomo",
-    version="0.0.7",
+    name="alfi",
+    version="1.0.0",
     author="Jacob Moss",
     author_email="cob.mossy@gmail.com",
-    description="A Latent Force Model library with variational and MCMC support for non-linear functions.",
+    description="An approximate latent force model library with variational inference for non-linear ODEs and PDEs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mossjacob/lafomo",
@@ -20,8 +20,8 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'tensorflow>=2.4.1',
-        'tensorflow-probability>=0.12.1',
+        # 'tensorflow>=2.4.1',
+        # 'tensorflow-probability>=0.12.1',
         'torch>=1.7.1',
         'torchdiffeq>=0.2.0',
         'pandas>=1.2.1',
