b8d420655a4e11f57cd9c8c92f10c5ef620c3f03
Jacob Moss <cob.mossy@gmail.com>
2021-06-14 12:34:19 +0100

    update rnavel script

--- experiments/model_specs/rnavelocity.py
+++ experiments/model_specs/rnavelocity.py
@@ -18,6 +18,8 @@ def build_rnavelocity(dataset, params, **kwargs):
     num_cells = dataset[0].shape[1]
     num_latents = 10
     num_inducing = 50  # (I x m x 1)
+    num_genes = 2000
+    num_outputs = num_genes * 2
     end_t = 12
     use_natural = False
 
@@ -39,8 +41,8 @@ def build_rnavelocity(dataset, params, **kwargs):
                                            gp_kwargs=dict(natural=use_natural))
 
     y_target = data
-    u_y = y_target[:2000]  # (num_genes, num_cells)
-    s_y = y_target[2000:]  # (num_genes, num_cells)
+    u_y = y_target[:num_genes]  # (num_genes, num_cells)
+    s_y = y_target[num_genes:]  # (num_genes, num_cells)
     x = s_y > torch.tensor(np.percentile(s_y, 98, axis=1)).unsqueeze(-1)
     s = s_y * x
     x = u_y > torch.tensor(np.percentile(u_y, 98, axis=1)).unsqueeze(-1)
@@ -53,7 +55,21 @@ def build_rnavelocity(dataset, params, **kwargs):
     gamma /= l2
     gamma = gamma.unsqueeze(-1)
     nonlinearity = softplus if params['nonlinearity'] == 'softplus' else relu
-    lfm = RNAVelocityLFM(4000, gp_model, config, nonlinearity=nonlinearity, decay_rate=gamma, num_training_points=num_cells)
+
+    nonzero_mask = list()
+    for gene_index in range(num_outputs // 2):
+        nonzero_mask.append(torch.logical_and(s_y[gene_index, :] > 0,
+                                              u_y[gene_index, :] > 0))
+    nonzero_mask = torch.stack(nonzero_mask).repeat(2, 1)
+
+    lfm_kwargs = dict(
+        nonlinearity=nonlinearity,
+        decay_rate=gamma,
+        num_training_points=num_cells,
+        nonzero_mask=nonzero_mask
+    )
+    lfm = RNAVelocityLFM(num_outputs, gp_model, config, **lfm_kwargs)
+
     if use_natural:
         variational_optimizer = NGD(lfm.variational_parameters(), num_data=num_cells, lr=0.05)
         parameter_optimizer = Adam(lfm.nonvariational_parameters(), lr=0.02)
