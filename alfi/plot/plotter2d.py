22d3d84be66d802ac53f576f3b2d1d040cd80908
Jacob Moss <cob.mossy@gmail.com>
2021-08-09 09:57:42 +0100

    plotter2d vector plot initial

--- alfi/plot/plotter2d.py
+++ alfi/plot/plotter2d.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import torch
 
 from .base_plotter import Plotter
 from alfi.datasets import scaled_barenco_data
@@ -24,95 +25,51 @@ class Plotter2d(Plotter):
         self.num_outputs = self.output_names.shape[0]
         self.num_replicates = self.model.num_outputs // self.num_outputs
 
-    def plot_gp(self, gp,
-                t_predict, t_scatter=None, y_scatter=None,
-                num_samples=0,
-                transform=lambda x:x,
-                ylim=None,
-                titles=None,
-                max_plots=10,
-                replicate=0,
-                only_plot_index=None,
-                ax=None,
-                plot_inducing=False,
-                color=Colours.line_color,
-                shade_color=Colours.shade_color):
-        """
-        Parameters:
-            gp: output distribution of LFM or associated GP models.
-            t_predict: tensor (T*,) prediction input vector
-            t_scatter: tensor (T,) target input vector
-            y_scatter: tensor (J, T) target output vector
-        """
-        mean = gp.mean.detach().transpose(0, 1)  # (T, J)
-        std = gp.variance.detach().transpose(0, 1).sqrt()
-        num_plots = mean.shape[0]
-        mean = mean.view(num_plots, self.num_replicates, -1).transpose(0, 1)[replicate].detach()
-        std = std.view(num_plots, self.num_replicates, -1).transpose(0, 1)[replicate].detach()
-        mean = transform(mean)
-        # std = transform(std)
-        num_plots = min(max_plots, num_plots)
-        axes_given = ax is not None
-        if not axes_given:
-            fig = plt.figure(figsize=(6, 4 * np.ceil(num_plots / 3)))
-        for i in range(num_plots):
-            if only_plot_index is not None:
-                i = only_plot_index
-            if not axes_given:
-                ax = fig.add_subplot(num_plots, min(num_plots, 3), i + 1)
-            if titles is not None:
-                ax.set_title(titles[i])
+    def plot_vector_gp(self, x1, x2, true_x1, true_x2,
+                       time_ass=None,
+                       ax=None,
+                       figsize=(3, 3),
+                       show=True,
+                       save_name=None,
+                       independent=True):
+        if not show:
+            plt.ioff()
 
-            ax.plot(t_predict, mean[i], color=color)
-            ax.fill_between(t_predict,
-                            mean[i] + 2 * std[i],
-                            mean[i] - 2 * std[i],
-                            color=shade_color, alpha=0.3)
+        if ax is None:
+            plt.figure(figsize=figsize)
+            ax = plt.subplot()
+        plt.title('lotka')
 
-            for _ in range(num_samples):
-                ax.plot(t_predict, transform(gp.sample().detach()).transpose(0, 1)[i], alpha=0.3, color=color)
+        # Plot trajectory
+        ax.plot(x1, x2, color='red')
+        ax.scatter(self.model.initial_state[:, 0], self.model.initial_state[:, 1])
 
-            if self.variational and plot_inducing:
-                inducing_points = self.model.inducing_points.detach()[0].squeeze()
-                ax.scatter(inducing_points, np.zeros_like(inducing_points), marker='_', c='black', linewidths=2)
-            if t_scatter is not None:
-                ax.scatter(t_scatter, y_scatter[replicate, i], color=Colours.scatter_color, marker='x')
-            if ylim is None:
-                lb = min(mean[i])
-                lb -= 0.2 * lb
-                ub = max(mean[i]) * 1.2
-                ax.set_ylim(lb, ub)
-            else:
-                ax.set_ylim(ylim)
+        # Plot cell points
+        indices = np.intersect1d(true_x1[:].nonzero(),
+                                 true_x2[:].nonzero())
 
-            if axes_given:
-                break
-        return gp
 
-    def plot_losses(self, trainer, last_x=50):
-        plt.figure(figsize=(5, 2))
-        plt.plot(np.sum(trainer.losses, axis=1)[-last_x:])
-        plt.title('Total loss')
-        plt.figure(figsize=(5, 2))
-        plt.subplot(221)
-        plt.plot(trainer.losses[-last_x:, 0])
-        plt.title('Loss')
-        plt.subplot(222)
-        plt.plot(trainer.losses[-last_x:, 1])
-        plt.title('KL-divergence')
+        ax.scatter(true_x1[indices], true_x2[indices],
+                   alpha=0.8, s=5, cmap='viridis', c=time_ass)
 
-    def plot_convergence(self, trainer):
-        titles = ['basal', 'decay', 'sensitivity', 'lengthscale']
-        datas = [np.array(trainer.basalrates)[:,:,0],
-                 np.array(trainer.decayrates)[:,:,0],
-                 np.array(trainer.sensitivities)[:,:,0],
-                 np.array(trainer.lengthscales)[:, 0, 0]]
+        # Plot inducing vectors
+        inducing_points = self.model.inducing_points
+        with torch.no_grad():
+            out = self.model.gp_model(inducing_points).mean
+        predator_grad = out[:, 0]
+        prey_grad = out[:, 1]
+        if independent:
+            predator = inducing_points[0, :, 0]
+            x2 = inducing_points[1, :, 0]
+        else:
+            predator = inducing_points[0, :, 0]
+            x2 = inducing_points[0, :, 1]
 
-        plt.figure(figsize=(5, 6))
-        for i, (title, data) in enumerate(zip(titles, datas)):
-            plt.subplot(411 + i)
-            plt.title(title)
-            # if data.ndim > 1:
-            #     for j in range(data.shape[1]):
+        ax.quiver(predator, x2, predator_grad, prey_grad)
 
-            plt.plot(data)
+        ax.set_ylabel('unspliced')
+        ax.set_xlabel('spliced')
+        if save_name is not None:
+            plt.savefig(save_name)
+        if not show:
+            plt.close(plt.gcf())
