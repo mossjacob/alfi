ae19ce1753e2a62f7f0db2ca91af1d79c28e1c1a
Jacob <cob.mossy@gmail.com>
2021-10-13 10:51:40 +0100

    remove rna velocity from alfi (SOC)

--- alfi/impl/odes/__init__.py
+++ alfi/impl/odes/__init__.py
@@ -1,11 +1,8 @@
-from .rna_velocity import RNAVelocityLFM, RNAVelocityConfiguration
 from .transcriptional import TranscriptionLFM
 from .lotka import LotkaVolterra, LotkaVolterraState
 
 
 __all__ = [
-    'RNAVelocityLFM',
-    'RNAVelocityConfiguration',
     'TranscriptionLFM',
     'LotkaVolterra',
     'LotkaVolterraState',
