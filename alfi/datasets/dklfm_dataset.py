dade37f45d5797ec652e746d1be089ba29a4a5eb
Jacob Moss <cob.mossy@gmail.com>
2023-09-23 17:24:49 +0100

    add scale option to DKLFMDataset

--- alfi/datasets/dklfm_dataset.py
+++ alfi/datasets/dklfm_dataset.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, TensorDataset
 
 
 class DeepKernelLFMDataset(Dataset):
@@ -23,7 +23,7 @@ class DeepKernelLFMDataset(Dataset):
     input_dim = 1
     """Dimensions of input. Default is 1. When 2, will plot input as images with output as colour."""
 
-    def __init__(self, x, y, f=None, train_indices=None, n_train=0.6, n_training_instances=0.5):
+    def __init__(self, x, y, f=None, train_indices=None, n_train=0.6, n_training_instances=0.5, scale_x_max=False, n_test_instances=1.):
         """
         Args:
             x: timepoints or inputs (num_inputs, dimensions)
@@ -49,13 +49,16 @@ class DeepKernelLFMDataset(Dataset):
         self.timepoints_cond = x[self.train_indices]
 
         self.x_cond = self.timepoints_cond.view(self.timepoints_cond.shape[0], -1).unsqueeze(0).repeat(self.n_task, 1, 1).type(torch.float64)
+        if scale_x_max:
+            self.x_cond /= self.x_cond.max()
         self.x_cond_blocks = self.x_cond.repeat(1, self.n_blocks, 1)
         shape = (self.n_task, -1)
         self.y_cond = y[..., self.train_indices].reshape(shape)  # (n_task, 2, T) -> (n_task, 2 * T)
         self.f_cond = f if f is None else f[..., self.train_indices].reshape(shape)
 
+        n_test_instances = int(n_test_instances * self.x_cond.shape[0]) if isinstance(n_test_instances, float) else n_test_instances
         self.train_instance_indices = np.arange(n_training_instances)
-        self.test_instance_indices = np.arange(n_training_instances, self.x_cond.shape[0])
+        self.test_instance_indices = np.arange(n_training_instances, n_training_instances + n_test_instances)
 
         self.train_x_cond_blocks = self.x_cond_blocks[self.train_instance_indices]
         self.train_x_cond = self.x_cond[self.train_instance_indices]
@@ -71,4 +74,12 @@ class DeepKernelLFMDataset(Dataset):
             self.train_x_cond[i],
             self.train_y_cond[i],
             self.train_f_cond[i]
+        )
+
+    def validation(self):
+        return TensorDataset(
+            self.x_cond_blocks[self.test_instance_indices],#self.train_x_cond_blocks,
+            self.x_cond[self.test_instance_indices], #self.train_x_cond,
+            self.y_cond[self.test_instance_indices], #self.train_y_cond,
+            self.f_cond[self.test_instance_indices] #self.train_f_cond
         )
